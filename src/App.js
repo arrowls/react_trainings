0e9ecc145e3488a3cc2c1bf40cc88169e1f9a736
arrowls <babuyoff@gmail.com>
2023-02-11 21:20:04 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+// React - лучшая реклама Vue
+
 import './App.css';
+import {useEffect, useRef, useState} from "react";
+import TrueDB from "./api/TrueDB";
+import { dateMap } from "./utils";
 
 function App() {
+  const dateRef = useRef();
+  const kmRef = useRef();
+  const [data, setData] = useState([]);
+
+  TrueDB.init();
+
+  useEffect(() => {
+    sort();
+  }, []);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    const date = dateRef.current.value;
+    let km = kmRef.current.value;
+    TrueDB.set(date, km);
+    sort()
+  }
+
+  function deleteHandler(id) {
+    TrueDB.delete(id);
+    sort();
+  }
+
+  function sort() {
+      const db = TrueDB.get();
+      const sorted = db.sort((a, b) => {
+        const prev = Number(a.date.split('-').join(''));
+        const curr = Number(b.date.split('-').join(''));
+        return prev < curr ? 1 : -1;
+      });
+
+      const normalized = sorted.map((item) => {
+        const parsedDate = item.date.split('-');
+        return {
+          id: item.date,
+          date: `${parsedDate[2]} ${dateMap[parsedDate[1]]} ${parsedDate[0]}`,
+          km: item.km,
+        }
+      });
+      setData(normalized)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <form id="form" onSubmit={submitHandler}>
+          <div className="form__row">
+            <label className="form__row-label">
+              <span className="form-description">Дата</span>
+              <input type="date" className="form__row-date__input" required={true} ref={dateRef} />
+            </label>
+            <label className="form__row-label">
+              <span className="form-description">Пройдено, км.</span>
+              <input type="number" className="form__row-date__input" required={true} ref={kmRef} />
+            </label>
+            <button type="submit">OK</button>
+          </div>
+        </form>
+        <div id="form-output">
+          <div className="form__row">
+            <span className="form__description">Дата</span>
+            <span className="form__description">Пройдено, км.</span>
+            <span className="form__description">Действия</span>
+          </div>
+          { data.map((el) =>
+              <div className="output__row" key={el.date}>
+                <span className="output__item">{el.date}</span>
+                <span className="output__item">{el.km}</span>
+                <span className="output__item delete-item" onClick={() => deleteHandler(el.id)}> X </span>
+              </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/api/TrueDB.js
@@ -0,0 +1,34 @@
+export default class TrueDB {
+    static init() {
+        if (!localStorage.getItem('trainings')) {
+            localStorage.setItem('trainings', '[]');
+        }
+    }
+
+    static set(date, km) {
+        const db = JSON.parse(localStorage.getItem('trainings')) || [];
+        if (this.has(date)) {
+            db[db.indexOf(this.get(date))].km += +km;
+        } else {
+            db.push({date, km: +km });
+        }
+        localStorage.setItem('trainings', JSON.stringify(db));
+    }
+
+    static has(date) {
+        const db = JSON.parse(localStorage.getItem('trainings')) || [];
+        return db.some((el) => el.date === date);
+    }
+
+    static get(date) {
+        const db = JSON.parse(localStorage.getItem('trainings')) || [];
+        if (!date) return db;
+        return db.find((el) => el.date === date);
+    }
+
+    static delete(date) {
+        let db = JSON.parse(localStorage.getItem('trainings')) || [];
+        db = db.filter((el) => el.date !== date);
+        localStorage.setItem('trainings', JSON.stringify(db));
+    }
+}
